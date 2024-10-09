2649126c5465f0d416d56cf5259bbc4857465478
Caspar Niekus <niekusc@ad.hva.nl>
2024-10-09 14:04:18 +0200

    Refactor web files to add HTML sliders for number of circles and rotation

--- web/sketch3.js
+++ web/sketch3.js
@@ -3,7 +3,6 @@
   let colors = [];
   let numCircles = 10;
   let rotationSpeed = 0.01;
-  let numCirclesSlider, rotationSpeedSlider;
 
   function setup() {
     let canvasWidth = windowWidth * 0.7;
@@ -14,21 +13,15 @@
     for (let i = 0; i < 10; i++) {
       colors.push([random(255), random(255), random(255)]);
     }
-
-    // Create sliders
-    numCirclesSlider = createSlider(1, 20, numCircles);
-    numCirclesSlider.position(10, 10);
-    rotationSpeedSlider = createSlider(0.001, 0.1, rotationSpeed, 0.001);
-    rotationSpeedSlider.position(10, 40);
   }
 
   function draw() {
     background(0);
     translate(width / 2, height / 2);
 
-    // Update parameters from sliders
-    numCircles = numCirclesSlider.value();
-    rotationSpeed = rotationSpeedSlider.value();
+    // Update parameters from HTML sliders
+    numCircles = parseInt(document.getElementById('numCircles').value);
+    rotationSpeed = parseFloat(document.getElementById('rotationSpeed').value);
 
     for (let i = 0; i < numCircles; i++) {
       push();
